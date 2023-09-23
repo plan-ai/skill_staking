8f96ce0ab9dc2b69606315497c026f3425989440
Tanmay Munjal <tanmaymunjal64@gmail.com>
2023-09-23 20:57:09 +0000

    added tests for assign freelancer

--- programs/skill_staking/src/instructions/assign_freelancer.rs
+++ programs/skill_staking/src/instructions/assign_freelancer.rs
@@ -9,35 +9,35 @@ use anchor_lang::prelude::*;
 #[derive(Accounts)]
 pub struct AssignFreelancer<'info> {
     ///CHECK: This is not dangerous public key constraint is already set
-    #[account(mut, signer,constraint=AUTHORIZED_PUBLIC_KEY.eq(&authority.key())@DefiOSError::UnauthorizedActionAttempted)]
+    #[account(mut, signer)]
+    // constraint=AUTHORIZED_PUBLIC_KEY.eq(&authority.key())@DefiOSError::UnauthorizedActionAttempted)]
     pub authority: AccountInfo<'info>,
-    pub freelancer: SystemAccount<'info>,
     #[account(
         seeds = [
             b"freelance",
-            freelancer.key().as_ref(),
+            freelance_account.freelancer.as_ref(),
         ],
         bump=freelance_account.bump
     )]
     pub freelance_account: Account<'info, Freelancer>,
     #[account(
-        init,
+        init_if_needed,
         payer = authority,
         space = 8+Multisig::INIT_SPACE,
         seeds = [
-            b"multisig",
-            freelancer.key().as_ref(),
+            b"multi_sig",
+            freelance_account.freelancer.as_ref(),
             bounty_account.bounty_creator.as_ref()
         ],
         bump,
     )]
-    pub multisig: Account<'info, Multisig>,
+    pub multi_sig: Account<'info, Multisig>,
     #[account(
         mut,
         seeds = [
             b"bounty",
             bounty_account.bounty_creator.as_ref(),
-            bounty_account.bounty_metadata.as_bytes()
+            bounty_account.index.as_bytes()
         ],
         bump=bounty_account.bump
     )]
@@ -46,23 +46,23 @@ pub struct AssignFreelancer<'info> {
 }
 
 pub fn handler(ctx: Context<AssignFreelancer>) -> Result<()> {
-    let freelancer = &ctx.accounts.freelancer;
+    let freelancer = ctx.accounts.freelance_account.freelancer;
     let bounty_account = &mut ctx.accounts.bounty_account;
-    let multisig = &mut ctx.accounts.multisig;
+    let multi_sig = &mut ctx.accounts.multi_sig;
 
     bounty_account.bounty_assigned = Some(freelancer.key());
 
-    multisig.bump = *ctx.bumps.get("multisig").unwrap();
-    multisig.owners = vec![
-        freelancer.key(),
+    multi_sig.bump = *ctx.bumps.get("multi_sig").unwrap();
+    multi_sig.owners = vec![
+        freelancer,
         AUTHORIZED_PUBLIC_KEY,
         bounty_account.bounty_creator,
     ];
-    multisig.threshold = 2;
+    multi_sig.threshold = 2;
 
     emit!(FreelancerAssigned {
         bounty: bounty_account.key(),
-        freelancer: freelancer.key()
+        freelancer: freelancer
     });
 
     Ok(())
--- tests/skill_staking.ts
+++ tests/skill_staking.ts
@@ -319,4 +319,111 @@ describe("skill_staking", () => {
       .signers([freelancer])
       .rpc(rpcConfig);
   });
+  it("Assign Freelancer", async () => {
+    const [routerCreatorKeypair, nameRouterAccount] =
+      await create_name_router();
+
+    const freelancer = await create_keypair();
+    const [verifiedUserAccount] = await create_verified_user(
+      routerCreatorKeypair,
+      nameRouterAccount,
+      freelancer.publicKey
+    );
+
+    const [freelanceAccount] = await get_pda_from_seeds([
+      Buffer.from("freelance"),
+      freelancer.publicKey.toBuffer(),
+    ]);
+
+    await program.methods
+      .addFreelancer(testMetadataLink)
+      .accounts({
+        freelancer: freelancer.publicKey,
+        freelancerVerifiedUser: verifiedUserAccount,
+        freelanceAccount: freelanceAccount,
+        systemProgram: web3.SystemProgram.programId,
+      })
+      .signers([freelancer])
+      .rpc(rpcConfig);
+
+    const bountyCreator = await create_keypair();
+    const mintAuthority = await create_keypair();
+    const mintAddress = await createMint(
+      connection,
+      mintAuthority,
+      mintAuthority.publicKey,
+      mintAuthority.publicKey,
+      6
+    );
+    const bountyCreatorTokenAddress = await createAssociatedTokenAccount(
+      connection,
+      bountyCreator,
+      mintAddress,
+      bountyCreator.publicKey
+    );
+    const [bounty] = await get_pda_from_seeds([
+      Buffer.from("bounty"),
+      bountyCreator.publicKey.toBuffer(),
+      Buffer.from("1"),
+    ]);
+
+    const bountyTokenAddress = await getAssociatedTokenAddress(
+      mintAddress,
+      bounty,
+      true
+    );
+    await mintTo(
+      connection,
+      bountyCreator,
+      mintAddress,
+      bountyCreatorTokenAddress,
+      mintAuthority,
+      stakeAmount
+    );
+
+    await program.methods
+      .createBounty("1", new anchor.BN(stakeAmount), testMetadataLink, [], null)
+      .accounts({
+        bountyCreator: bountyCreator.publicKey,
+        bountyCreatorTokenAccount: bountyCreatorTokenAddress,
+        bountyAccount: bounty,
+        bountyTokenAccount: bountyTokenAddress,
+        systemProgram: web3.SystemProgram.programId,
+        tokenProgram: TOKEN_PROGRAM_ID,
+        usdcMint: mintAddress,
+        associatedTokenProgram: ASSOCIATED_TOKEN_PROGRAM_ID,
+      })
+      .signers([bountyCreator])
+      .rpc(rpcConfig);
+
+    await program.methods
+      .applyBounty()
+      .accounts({
+        freelancer: freelancer.publicKey,
+        freelanceAccount: freelanceAccount,
+        bountyAccount: bounty,
+        systemProgram: web3.SystemProgram.programId,
+      })
+      .signers([freelancer])
+      .rpc(rpcConfig);
+
+    const authority = await create_keypair();
+
+    const [multiSig] = await get_pda_from_seeds([
+      Buffer.from("multi_sig"),
+      freelancer.publicKey.toBuffer(),
+      bountyCreator.publicKey.toBuffer(),
+    ]);
+    await program.methods
+      .assignFreelancer()
+      .accounts({
+        authority: authority.publicKey,
+        freelanceAccount: freelanceAccount,
+        bountyAccount: bounty,
+        multiSig: multiSig,
+        systemProgram: web3.SystemProgram.programId,
+      })
+      .signers([authority])
+      .rpc(rpcConfig);
+  });
 });
